716ce2ca084a06eeb919905df628a91759808f24
Raghad <121506944+Raghad-Aldakhil@users.noreply.github.com>
2023-02-12 01:50:59 +0300

    Add files via upload

---
+++ Project/src/mongodb.js
@@ -0,0 +1,30 @@
+const mongoose=require("mongoose");
+const mongoose=require("express");
+
+//make the data base
+mongoose.set("strictQuery",true);
+mongoose.connect("mongodb://localhost:27017/loginData")
+
+// console if cnnected or not
+.then(()=>{
+    console.log("mngodb connected");
+})
+.catch(()=>{
+    console.log("failed to connect mongodb");
+})
+// making the database schema for login information 
+const LogInSchema=new mongoose.Schema({
+    name : {
+        type:String,
+        required:true
+    },
+    password :{
+        type:String,
+        required:true
+    }
+
+});
+
+// collection 
+const collection = new mongoose.model("Collection1",LogInSchema)
+module.exports=collection
